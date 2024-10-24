d3d2d59dee455635947709c8e0a8e5763d469e33
RuslanGon <ruslan441006@gmail.com>
2024-10-24 17:13:43 +0300

    wer

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,14 +2,17 @@
     <div>
       <h1>Hello</h1>
       <User />
+      <Input />
     </div>
   </template>
   
   <script>
   import User from './components/User.vue'
+  import Input from './components/Input.vue'
+
   
   export default {
-    components: { User },
+    components: { User, Input },
     data() {
       return {
         
