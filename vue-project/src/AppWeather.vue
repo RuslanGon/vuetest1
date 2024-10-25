92aeb8ba8a3b62a771b342c6cc832dc2098d54b8
RuslanGon <ruslan441006@gmail.com>
2024-10-25 16:23:53 +0300

    wer

---
+++ vue-project/src/AppWeather.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="wrapper">
+<h1>Weather aplication</h1>
+<p>find out the weather in your city</p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/weather.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './AppWeather.vue'
+
 
 createApp(App).mount('#app')
