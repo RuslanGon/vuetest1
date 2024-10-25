871999a700aef6db2730c47b96472268f3223f85
RuslanGon <ruslan441006@gmail.com>
2024-10-25 16:39:10 +0300

    wer

--- vue-project/src/AppWeather.vue
+++ vue-project/src/AppWeather.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="wrapper">
-<h1>Weather aplication</h1>
-<p>find out the weather in your city</p>
+<h1 class="tittle">Weather aplication</h1>
+<p class="text">find out the weather in your city</p>
+<input type="text" name="" placeholder="enter city">
+<button>Get weather</button>
 </div>
 </template>
 
@@ -12,5 +14,29 @@ export default {
 </script>
 
 <style scoped>
+input {
+    width: 300px;
+    height: 40px;
+border-radius: 10px;
+color: white;
+background-color: transparent;
+border-bottom: 3px solid gold;
+font-size: 24px;
+}
+.text {
+    margin-bottom: 30px;
+}
+.tittle {
+margin-top: 50px;
+margin-bottom: 30px;
+}
+.wrapper {
+    width: 900px;
+    height: 500px;
+    border-radius: 50px;
+    border: 2px solid white;
+    padding: 20px;
+    text-align: center;
+}
 
 </style>
